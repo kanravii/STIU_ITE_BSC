88aa0543b7f8081209133dafe9afb347d34c2120
Kanravi <2402290003@students.stamford.edu>
2026-09-19 07:34:02 +0700

    first commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const dns = require("node:dns");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+    }
+};
+
+module.exports = connectDB;
